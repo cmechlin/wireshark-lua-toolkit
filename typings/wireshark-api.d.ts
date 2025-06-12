2082e7913e4837f1291ed09a83002f69ddbbffb4
cmechlin <cmechlin@gmail.com>
2025-06-12 16:58:40 -0400

    feat: add Wireshark Lua API typings and IntelliSense scaffold

--- typings/wireshark-api.d.ts
+++ typings/wireshark-api.d.ts
@@ -4,49 +4,49 @@
  */
 
 declare interface Proto {
-  name: string
-  description: string
-  fields: ProtoField[]
-  dissector: (buffer: Tvb, pinfo: Pinfo, tree: TreeItem) => void
+  name: string;
+  description: string;
+  fields: ProtoField[];
+  dissector: (buffer: Tvb, pinfo: Pinfo, tree: TreeItem) => void;
 }
 
 declare interface ProtoField {
-  name: string
-  abbr: string
-  type: string
-  base: number
-  value?: any
+  name: string;
+  abbr: string;
+  type: string;
+  base: number;
+  value?: any;
 }
 
 declare interface Field {
-  new(name: string, abbr: string, type: string, base: number): ProtoField
+  new (name: string, abbr: string, type: string, base: number): ProtoField;
 }
 
 declare interface Listener {
-  new(protoName: string): Listener
-  remove(): void
+  new (protoName: string): Listener;
+  remove(): void;
 }
 
 declare interface Tap {
-  new(name: string): Tap
-  remove(): void
+  new (name: string): Tap;
+  remove(): void;
 }
 
 declare interface Tvb {
-  len: number
-  raw(offset: number, length: number): Uint8Array
+  len: number;
+  raw(offset: number, length: number): Uint8Array;
 }
 
 declare interface Pinfo {
-  src_port: number
-  dst_port: number
-  match_uint: number
+  src_port: number;
+  dst_port: number;
+  match_uint: number;
 }
 
 declare interface TreeItem {
-  add(protoField: ProtoField, value: any): TreeItem
+  add(protoField: ProtoField, value: any): TreeItem;
 }
 
-declare const register_postdissector: (fn: () => void) => void
+declare const register_postdissector: (fn: () => void) => void;
 
-declare const register_init: (fn: () => void) => void
+declare const register_init: (fn: () => void) => void;
